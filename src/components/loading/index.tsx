ec8ae4fa64a292135ba16c7a36cf08eba2449a7a
Liaess <yann.melo@gmail.com>
2022-03-05 11:30:24 -0300

    feat: add loading component

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,20 @@
+import { TailSpin } from "react-loader-spinner";
+import styled from "styled-components";
+
+export default function Loading() {
+  return (
+    <>
+      <Container>
+        <TailSpin color="var(--white-color)" height={100} width={100} />
+      </Container>
+      ;
+    </>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
