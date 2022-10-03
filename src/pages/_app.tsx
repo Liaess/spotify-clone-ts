4fbbe714eef905d050eb26c10853229baa0ea94e
Liaess <yann.melo@gmail.com>
2022-10-03 02:10:07 -0300

    fix: sesson props after update dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,13 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Session } from "next-auth";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{session: Session}>) {
   return (
     <>
       <SongProvider>
-        <SessionProvider session={session}>
+        <SessionProvider session={pageProps.session}>
           <ToastContainer />
           <GlobalStyle />
           <Component {...pageProps} />
